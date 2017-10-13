5e5afed3ab1f5162c1dfd19cc30311cf8149a760
tjandrayana <tjandrayana.setiawan@ti.ukdw.ac.id>
2017-10-14 00:51:08 +0700

    try send

--- main.go
+++ main.go
@@ -1,22 +1,22 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
+	"net/url"
 	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/line/line-bot-sdk-go/linebot"
 )
 
-var bot *linebot.Client
-
 func main() {
-	bot, err := linebot.New(os.Getenv("channelSecret"), os.Getenv("channelAccessToken"))
-
-	log.Println("Bot:", bot, " err:", err)
 
 	r := gin.New()
+	r.Use(gin.Logger())
 	r.GET("/ping", ping)
 
 	r.POST("/line/triger", Triger)
@@ -34,27 +34,103 @@ func ping(c *gin.Context) {
 // ParseRequest func
 func Triger(c *gin.Context) {
 
-	events, err := bot.ParseRequest(c.Request)
-
-	if err != nil {
-		if err == linebot.ErrInvalidSignature {
-			c.Writer.WriteHeader(400)
-		} else {
-			c.Writer.WriteHeader(500)
-		}
+	var botRequest BotRequest
+	if c.Bind(&botRequest) == nil {
+		c.JSON(http.StatusOK, gin.H{"status": fmt.Sprintf("request convert error, request data is %+v", botRequest)})
 		return
 	}
 
-	for _, event := range events {
-		fmt.Printf("\n%+v\n", event)
-		if event.Type == linebot.EventTypeMessage {
-			switch message := event.Message.(type) {
-			case *linebot.TextMessage:
-				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.ID+":"+message.Text+" OK!")).Do(); err != nil {
-					log.Print(err)
-				}
-			}
+	for _, result := range botRequest.Result {
+
+		request := SendRequest{
+			To:        []string{result.Content.From},
+			ToChannel: ToChannel,
+			EventType: EventType,
+			Content: Content{
+				ContentType: result.Content.ContentType,
+				ToType:      result.Content.ToType,
+				Text:        result.Content.Text,
+			},
+		}
+
+		if _, err := post(request); err != nil {
+			log.Printf("Error: %s", err.Error())
 		}
 	}
 
+	c.JSON(http.StatusOK, gin.H{"status": "end"})
+
+}
+
+type BotRequestBody struct {
+	Request BotRequest `json:"Request"`
+}
+
+//BotRequest
+type BotRequest struct {
+	Result []BotResult `json:"result"`
+}
+
+type BotResult struct {
+	From        string   `json:"from"`
+	FromChannel string   `json:"fromChannel"`
+	To          []string `json:"to"`
+	ToChannel   string   `json:"toChannel"`
+	EventType   string   `json:"eventType"`
+	ID          string   `json:"id"`
+	Content     Content  `json:"content"`
+}
+
+type Content struct {
+	ID          string   `json:"id"`
+	ContentType int      `json:"contentType"`
+	From        string   `json:"from"`
+	CreatedTime int      `json:"createdTime"`
+	To          []string `json:"to"`
+	ToType      int      `json:"toType"`
+	Text        string   `json:"text"`
+}
+
+type SendRequest struct {
+	To        []string `json:"to"`
+	ToChannel int      `json:"toChannel"`
+	EventType string   `json:"eventType"`
+	Content   Content  `json:"content"`
+}
+
+const (
+	EndPoint  = "tjandrayana-line-bot.herokuapp.com/line"
+	ToChannel = 1540625385
+	EventType = "138311608800106203"
+)
+
+func callbackHandler(c *gin.Context) {
+
+}
+
+func post(r SendRequest) (*http.Response, error) {
+	b, _ := json.Marshal(r)
+	req, _ := http.NewRequest(
+		"POST",
+		EndPoint,
+		bytes.NewBuffer(b),
+	)
+
+	req = setHeader(req)
+
+	proxyURL, _ := url.Parse(os.Getenv("FIXIE_URL"))
+	client := &http.Client{
+		Timeout:   time.Duration(15 * time.Second),
+		Transport: &http.Transport{Proxy: http.ProxyURL(proxyURL)},
+	}
+
+	return client.Do(req)
+}
+
+func setHeader(req *http.Request) *http.Request {
+	req.Header.Add("Content-Type", "application/json; charset=UTF-8")
+	req.Header.Add("X-Line-ChannelID", os.Getenv("LINE_CHANNEL_ID"))
+	req.Header.Add("X-Line-ChannelSecret", os.Getenv("LINE_CHANNEL_SECRET"))
+	req.Header.Add("X-Line-Trusted-User-With-ACL", os.Getenv("LINE_CHANNEL_MID"))
+	return req
 }
