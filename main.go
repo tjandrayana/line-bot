f8d030202f705c287210f5ac7ea8a7447a6d359f
tjandrayana <tjandrayana.setiawan@ti.ukdw.ac.id>
2017-10-13 19:17:03 +0700

    add route

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 
 	fmt.Println("Halo")
+	http.HandleFunc("/index/", viewHandler)
+	http.ListenAndServe(":1000", nil)
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+
+	a := []byte("test : aaa")
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(a)
 
 }
