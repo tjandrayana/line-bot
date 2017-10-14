81f5c13a48349caa75bbb84fa4b10bcc1e2785aa
tjandrayana <tjandrayana.setiawan@ti.ukdw.ac.id>
2017-10-14 10:14:27 +0700

    add push message

--- main.go
+++ main.go
@@ -46,7 +46,6 @@ func main() {
 
 	r := gin.New()
 	r.GET("/ping", ping)
-	r.GET("/send", pushMessage)
 
 	r.POST("/line/triger", Triger)
 
@@ -58,107 +57,6 @@ func ping(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"message": "pong",
 	})
-
-}
-
-// func triger(c *gin.Context) {
-
-// 	body, err := ioutil.ReadAll(c.Request.Body)
-// 	if err != nil {
-// 		log.Println("Error : ", err)
-// 		return
-// 	}
-
-// 	c.JSON(200, gin.H{
-// 		"message": string(body),
-// 	})
-
-// }
-
-func pushMessage(c *gin.Context) {
-	defer c.Request.Body.Close()
-	channelSecret := os.Getenv("channelSecret")
-
-	body, err := ioutil.ReadAll(c.Request.Body)
-	if err != nil {
-		log.Println("error : ", err)
-		return
-	}
-
-	fmt.Printf("\n%+v\n", string(body))
-
-	if !validateSignature(channelSecret, c.Request.Header.Get("X-Line-Signature"), body) {
-		log.Println("error : ", err)
-		return
-	}
-
-	var dat Data
-	if err := json.Unmarshal(body, &dat); err != nil {
-		log.Println(err)
-	}
-
-	if err := reply(dat); err != nil {
-		log.Println("ERROR = ", err)
-	}
-
-	fmt.Println("\nSuccess\n")
-
-}
-
-func validateSignature(channelSecret, signature string, body []byte) bool {
-	decoded, err := base64.StdEncoding.DecodeString(signature)
-	if err != nil {
-		return false
-	}
-	hash := hmac.New(sha256.New, []byte(channelSecret))
-	hash.Write(body)
-	return hmac.Equal(decoded, hash.Sum(nil))
-}
-
-type Reply struct {
-	ReplyToken string    `json:"replyToken"`
-	Messages   []Message `json:"messages"`
-	To         string    `json:"to,omitempty"`
-}
-
-func reply(dat Data) error {
-
-	mess1 := Message{
-		Type: "text",
-		Text: "Hai User ... ",
-	}
-
-	mess2 := Message{
-		Type: "text",
-		Text: "May I help you ...? ",
-	}
-
-	messages := []Message{mess1, mess2}
-
-	rep := Reply{
-		ReplyToken: dat.Events[0].ReplyToken,
-		Messages:   messages,
-		To:         "mikhael73",
-	}
-
-	agent := utils.NewHTTPRequest()
-	agent.Url = "https://api.line.me"
-	agent.Path = "/v2/bot/message/push"
-	agent.Method = "POST"
-	agent.IsJson = true
-	agent.Json = rep
-
-	agent.Headers["Authorization"] = "Bearer " + os.Getenv("channelAccessToken")
-
-	body, err := agent.DoReq()
-	if err != nil {
-		log.Println(err)
-		return err
-	}
-
-	fmt.Printf("\n Body : %+v\n", string(*body))
-
-	return nil
 }
 
 // ParseRequest func
@@ -185,9 +83,107 @@ func Triger(c *gin.Context) {
 	}
 
 	if err := reply(dat); err != nil {
-		log.Println("ERROR = ", err)
+		log.Println("Reply ERROR = ", err)
+	}
+
+	if err := pushMessage(dat); err != nil {
+		log.Println("Push Message ERROR = ", err)
 	}
 
 	fmt.Println("\nSuccess\n")
 
+}
+
+func validateSignature(channelSecret, signature string, body []byte) bool {
+	decoded, err := base64.StdEncoding.DecodeString(signature)
+	if err != nil {
+		return false
+	}
+	hash := hmac.New(sha256.New, []byte(channelSecret))
+	hash.Write(body)
+	return hmac.Equal(decoded, hash.Sum(nil))
+}
+
+type Reply struct {
+	ReplyToken string    `json:"replyToken,omitempty"`
+	Messages   []Message `json:"messages"`
+	To         string    `json:"to,omitempty"`
+}
+
+func reply(dat Data) error {
+
+	mess1 := Message{
+		Type: "text",
+		Text: "Hai User ... ",
+	}
+
+	mess2 := Message{
+		Type: "text",
+		Text: "May I help you ...? ",
+	}
+
+	messages := []Message{mess1, mess2}
+
+	rep := Reply{
+		ReplyToken: dat.Events[0].ReplyToken,
+		Messages:   messages,
+	}
+
+	agent := utils.NewHTTPRequest()
+	agent.Url = "https://api.line.me"
+	agent.Path = "/v2/bot/message/reply"
+	agent.Method = "POST"
+	agent.IsJson = true
+	agent.Json = rep
+
+	agent.Headers["Authorization"] = "Bearer " + os.Getenv("channelAccessToken")
+
+	body, err := agent.DoReq()
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	fmt.Printf("\n Body : %+v\n", string(*body))
+
+	return nil
+}
+
+func pushMessage(dat Data) error {
+
+	mess1 := Message{
+		Type: "text",
+		Text: "Hai User ... ",
+	}
+
+	mess2 := Message{
+		Type: "text",
+		Text: "May I help you ...? ",
+	}
+
+	messages := []Message{mess1, mess2}
+
+	rep := Reply{
+		To:       "mikhael73",
+		Messages: messages,
+	}
+
+	agent := utils.NewHTTPRequest()
+	agent.Url = "https://api.line.me"
+	agent.Path = "/v2/bot/message/push"
+	agent.Method = "POST"
+	agent.IsJson = true
+	agent.Json = rep
+
+	agent.Headers["Authorization"] = "Bearer " + os.Getenv("channelAccessToken")
+
+	body, err := agent.DoReq()
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	fmt.Printf("\n Body : %+v\n", string(*body))
+
+	return nil
 }
